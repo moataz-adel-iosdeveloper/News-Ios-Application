ce57a2cb23b5b9fab2d4b6a21e2d95745dc9e1b2
moataz-adel-iosdeveloper <moataz_adel2010@yahoo.com>
2022-03-07 14:30:23 +0200

    Initial Commit

---
+++ NewsApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApplication
+//
+//  Created by ModyMayaAser on 07/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
